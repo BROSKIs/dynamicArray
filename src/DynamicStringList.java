d79f96801d9a54fcb4e41eeb86226a72c9afa904
Pasha <pavloyousee@gmail.com>
2026-04-23 11:40:21 -0700

    notwrking

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -1,9 +1,10 @@
 public class DynamicStringList implements StringList{
+    int size;
+    int capacity;
+    String[] data;
 
     public String get(int index){
-        // int size = 0;
-        // int capacity = 10;
-        // String[] data = new String[capacity];
+        
     }
 
     public void set(int index, String value){
@@ -11,7 +12,15 @@ public class DynamicStringList implements StringList{
     }
 
     public void add(String value){
+        if(!(capacity()-size()>0)){
+            //double our array size
+        }
 
+        //add the value to the index of size
+        data[size] = value;
+
+        size++;
+        //size++
     }
 
     public String remove(int index){
@@ -19,14 +28,16 @@ public class DynamicStringList implements StringList{
     }
 
     public int size(){
-
+        return size;
     }
 
     public int capacity(){
-
+        return capacity;
     }
 
     public static void main(String[] args){
-
+        size = 0;
+        capacity = 10;
+        data = new String[capacity];
     }
 }
