af7d61bcd4aee395d60db67007246c03cafef853
sodatax <[200~62825932+sodatax@users.noreply.github.com~>
2026-04-23 11:33:17 -0700

    set up DynamicStringList class

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -1,8 +1,32 @@
-public class DynamicStringList {
+public class DynamicStringList implements StringList{
 
-    public void DynamicStringList(){
-        int size = 0;
-        int capacity = 10;
-        String[] data = new String[capacity];
+    public String get(int index){
+        // int size = 0;
+        // int capacity = 10;
+        // String[] data = new String[capacity];
+    }
+
+    public void set(int index, String value){
+
+    }
+
+    public void add(String value){
+
+    }
+
+    public String remove(int index){
+
+    }
+
+    public int size(){
+
+    }
+
+    public int capacity(){
+
+    }
+
+    public static void main(String[] args){
+
     }
 }
